06893721bbeead51bab016d94dab31657efc05e2
Michael Allan <michaelallan@optushome.com.au>
2014-12-02 17:51:43 +1100

    Rewrite as Object Oriented

--- app.js
+++ app.js
@@ -1,81 +1,99 @@
 var Table = require('cli-table');
 
+var spins = require('./data/spins');
+
 var ranges = [
-    [1,2,3,4,5,6],
-    [7,8,9,10,11,12],
+    [ 1, 2, 3, 4, 5, 6],
+    [ 7, 8, 9,10,11,12],
     [13,14,15,16,17,18],
     [19,20,21,22,23,24],
     [25,26,27,28,29,30],
     [31,32,33,34,35,36]
 ];
 
-var spins = require('./data/spins');
+var Game = function (target, range, start) {
 
-var i = 250;
-var target = 180;
-var cashout = target;
-var divisor = 12;
-var wager = 5;
-var result = 0;
-var progress = 0;
-var losses = 0;
-var rounds = 0;
+    this.target = target || 60; // Target for total winnings
+    this.range = range || 0;    // Choose the range to bet on
+    this.start = start || 0;    // Start at the first random number unless specified
+    this.cashout = this.target; // Store original target value as cashout figure
 
-var spin, collect;
+    // Starting values
+    this.divisor = 12;
+    this.wager = 5;
+    this.result = 0;
+    this.progress = 0;
 
-function chooseNewRange(oldRange) {
-    // pick an index for the ranges above
-    // if the previous range is specified, recurse until a non-colliding value can be returned
-    var rand = Math.floor(Math.random() * 6);
-    return (oldRange && rand === oldRange) ?
-        chooseNewRange(oldRange) : rand;
-}
+    // Display results
+    this.table = new Table({
+        head: ['Range', 'Target', 'Divisor', 'Bet', 'Spin', 'Result', 'Progress']
+    });
 
-var range = chooseNewRange();
+    // Track game stats
+    this.losses = 0;
+    this.rounds = 0;
+};
 
-var table = new Table({
-    head: ['Range', 'Target', 'Divisor', 'Bet', 'Spin', 'Result', 'Progress']
-});
+Game.prototype.play = function () {
+    var i = this.start;
 
-function toNearestFiveCeil(num) {
-    return Math.ceil(num / 5) * 5;
-}
+    while (this.progress < this.cashout) {
+        this.spinTheWheel(i++);
+    }
 
-// Now, play until our cashout target is reached
-while (progress < cashout) {
+    this.displayResults();
 
-    // Spin the wheel
-    spin = spins[i];
+    // TODO collect stats
+};
 
-    // If spin is in the range, add to kitty, otherwise subtract
-    result = ranges[range].indexOf(spin) !== -1 ? wager * 5 : -wager;
+Game.prototype.displayResults = function () {
+    console.log(this.table.toString());
+};
+
+Game.prototype.spinTheWheel = function (index) {
+
+    // Grab the next number from the random series
+    var spin = spins[index];
+
+    // If spin is in the range, add winnings to kitty, otherwise subtract wager
+    this.result = ranges[this.range].indexOf(spin) !== -1 ? this.wager * 5 : -this.wager;
 
     // Add the result to our tally
-    progress += result;
+    this.progress += this.result;
 
-    // Display results of this spin
-    table.push([
-        range + 1, target, divisor, wager, spin, result, progress
+    // Add the results of this spin to our table
+    this.table.push([
+        this.range + 1, this.target, this.divisor, this.wager, spin, this.result, this.progress
     ]);
 
     // Modify our target to accommodate losses
-    target = target - result;
+    this.target = this.target - this.result;
 
     // Note if this is a winning round
-    collect = result > 0;
+    this.collect = this.result > 0;
 
     // Count successive losses
-    losses = collect ? 0 : losses++;
+    this.losses = this.collect ? 0 : this.losses++;
 
-    if (collect) {
+    if (this.collect) {
         // Modify our stakes if we've had a collect
-        divisor = divisor - 2;
+        this.divisor = this.divisor - 2;
     }
 
     // Modify our wager
-    wager = toNearestFiveCeil(target / divisor);
+    this.wager = this.roundUpToNearestFive(this.target / this.divisor);
+};
 
-    i++;
-}
+Game.prototype.roundUpToNearestFive = function (num) {
+    return Math.ceil(num / 5) * 5;
+};
 
-console.log(table.toString());
+
+
+// Play each range to see the result
+[0,1,2,3,4,5].forEach(function (range) {
+    var game = new Game(180, range, 50);
+    game.play();
+});
+
+
