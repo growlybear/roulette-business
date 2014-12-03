ea0b0390c42599cebe850f6917c2845a7e96ca70
Michael Allan <michaelallan@optushome.com.au>
2014-12-03 12:36:09 +1100

    Display raw data for game on page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,25 +6,68 @@ var React = window.React = require('react');
 var ranges = require('../data/ranges');
 var spins = require('../data/spins');
 
+var Game = require('./Game');
+
 var Header = require('./ui/Header.react');
 var Main = require('./ui/Main.react');
 var Footer = require('./ui/Footer.react');
 
 var RouletteApp = React.createClass({
-
-    play: function (ev) {
-        window.alert('boo!');
+    getDefaultProps: function () {
+        return {
+            ranges: ranges,     // Range of 6 numbers to play (1-6, 2-12, etc.)
+            spins: spins        // Random number data
+        };
     },
+    getInitialState: function () {
+        return {
+            target: 60,
+            range: 0,
+            wager: 5,
+            startSpinsAt: 0,    // Starting point in our 10,000 random number array
+            games: [],
+            numbers: []
+        };
+    },
+    componentWillMount: function () {
+        var that = this;
 
+        // Play a game for each range and store the results in state
+        [0,1,2,3,4,5].forEach(function (range) {
+            // Get results for each game
+            var game = new Game(that.state.target, that.state.range, that.state.wager);
+            game.play();
+
+            that.setState({games: that.state.games.concat([game.results()])});
+
+            // Get global stats
+            var numbers = game.getNumbers();
+            if (numbers.length > that.state.numbers.length) {
+                that.setState({ numbers: numbers });
+            }
+        });
+    },
+    /**
+        <div className="container">
+            <Header />
+            <GameNumbers />
+            <GameStats />
+            <Games />
+            <Footer />
+        </div>
+    */
     render: function () {
         return (
             <div className="container">
                 <Header />
-                <Main />
+                { this.state.games }
+                <p className="bg-success">{ this.state.numbers }</p>
                 <Footer />
             </div>
         );
     }
 });
 
-React.render(<RouletteApp />, document.getElementById('app'));
+
+
+React.render(<RouletteApp ranges={ ranges } spins={ spins } />, document.getElementById('app'));
